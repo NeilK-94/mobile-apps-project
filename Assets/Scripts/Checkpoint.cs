e4f9ae0ab02edb59b7d0f6324dc3279d13c5e844
NeilK-94 <neil.kyne@gmail.com>
2016-04-02 12:59:07 +0100

    j

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Checkpoint : MonoBehaviour {
+
+    public LevelManager levelManager;   //  create empty level manager of type LevelManager
+
+    // Use this for initialization
+    void Start()
+    {
+        levelManager = FindObjectOfType<LevelManager>();    //  finds any objects in scene that have LevelManager with it
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "Player")
+        {
+            levelManager.currentCheckpoint = gameObject;
+            Debug.Log(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+    public GameObject currentCheckpoint;
+    private PlayerController player;
+
+	// Use this for initialization
+	void Start () {
+        player = FindObjectOfType<PlayerController>();  //  find player controller in scene and assign it to the player
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void RespawnPlayer()
+    {
+        player.transform.position = currentCheckpoint.transform.position;
+    }
+}
